7db96b355ad446e30d698799125d22948daafb29
JaechulLee55 <155716835+JaechulLee55@users.noreply.github.com>
2024-06-07 08:38:34 +0200

    Update app.py

--- project/techtrends/app.py
+++ project/techtrends/app.py
@@ -1,19 +1,18 @@
 import sqlite3
- 
+
 from flask import Flask, jsonify, json, render_template, request, url_for, redirect, flash
 from werkzeug.exceptions import abort
 import logging
- 
+import sys
+
 # Function to get a database connection.
 # This function connects to database with the name `database.db`
-db_connection_count = 0
 def get_db_connection():
-    global db_connection_count
-    db_connection_count += 1
     connection = sqlite3.connect('database.db')
     connection.row_factory = sqlite3.Row
+    app.config['db_count'] += 1
     return connection
- 
+
 # Function to get a post using its ID
 def get_post(post_id):
     connection = get_db_connection()
@@ -21,46 +20,21 @@ def get_post(post_id):
                         (post_id,)).fetchone()
     connection.close()
     return post
- 
+
 # Define the Flask application
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your secret key'
+app.config['db_count'] = 0
 
-#######step1 creat healthz, metrics and log 
-# Define Healthz
-@app.route('/healthz')
-def status():
-    response = app.response_class(
-            response=json.dumps({"result":"OK - healthy"}),
-            status=200,
-            mimetype='application/json'
-    )
-    return response
- 
-# Define Metrics
-@app.route('/metrics')
-def metrics():
-    connect = get_db_connection()
-    posts = connect.execute('SELECT * FROM posts').fetchall()
-    post_count = len(posts)
-    response = app.response_class(
-            response=json.dumps({"data":
-            {"db_connection_count": db_connection_count, "post_count": post_count}}),
-            status=200,
-            mimetype='application/json'
-    )
- 
-    return response
- 
-# Define the main route of the web application
+# Define the main route of the web application 
 @app.route('/')
 def index():
     connection = get_db_connection()
     posts = connection.execute('SELECT * FROM posts').fetchall()
     connection.close()
     return render_template('index.html', posts=posts)
- 
-# Define how each individual article is rendered
+
+# Define how each individual article is rendered 
 # If the post ID is not found a 404 page is shown
 @app.route('/<int:post_id>')
 def post(post_id):
@@ -69,19 +43,19 @@ def post(post_id):
       return render_template('404.html'), 404
     else:
       return render_template('post.html', post=post)
- 
+
 # Define the About Us page
 @app.route('/about')
 def about():
     return render_template('about.html')
- 
-# Define the post creation functionality
+
+# Define the post creation functionality 
 @app.route('/create', methods=('GET', 'POST'))
 def create():
     if request.method == 'POST':
         title = request.form['title']
         content = request.form['content']
- 
+
         if not title:
             flash('Title is required!')
         else:
@@ -90,13 +64,43 @@ def create():
                          (title, content))
             connection.commit()
             connection.close()
- 
+
             return redirect(url_for('index'))
- 
+
     return render_template('create.html')
- 
+
+# Define the Healthz
+@app.route('/healthz')
+def healthz():
+    return app.response_class(
+        response=json.dumps({"result":"OK - healthy"}),
+        status=200,
+        mimetype='application/json'
+    )
+
+# Define the Metrics
+@app.route('/metrics')
+def metrics():
+    # Connect to database
+    connection = get_db_connection()
+    posts = connection.execute('SELECT * FROM posts').fetchall()
+    connection.close()
+
+    # Response 
+    response = app.response_class(
+        response=json.dumps({"data": {
+            "db_connection_count": app.config['db_count'], 
+            "post_count": len(posts)
+        }
+        }),
+        status=200,
+        mimetype='application/json'
+    )
+
+    return response
+
+
 # start the application on port 3111
 if __name__ == "__main__":
-    ## stream logs to app.log file
-    logging.basicConfig(filename='app.log',level=logging.DEBUG)
-    app.run(host='0.0.0.0', port='3113')
+   logging.basicConfig(level=logging.DEBUG, handlers=[logging.FileHandler("app.log"), logging.StreamHandler(sys.stdout)])
+   app.run(host='0.0.0.0', port='3111')
